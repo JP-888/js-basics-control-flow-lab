e15909b7f66caa9777addb41fbf2ce41c278045c
JP <jpsimshed@gmail.com>
2021-05-14 12:34:03 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+ if (distance <= 400)
+  return 'This one is on me!'
+
+ if (distance > 2500) 
+  return 'No can do.' 
+
+ if (distance > 2000) 
+  return 'I will gladly take your thirty bucks.'
+}  
+
+function ternaryCheckCity(city) {
+ if (city === 'NYC')
+  return 'Ok, sounds good.'
+
+ if (city !== 'NYC')
+  return 'No go.'
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip) {
+ if (tip === 'generous')
+  return 'Thank you so much.'
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+ if (tip === 'not as generous') 
+  return 'Thank you.'
+
+ if (tip = 'if anything else') 
+  return 'Bye.'
 }
